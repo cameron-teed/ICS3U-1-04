451c6032f34622ceff3902d5e475c100268fdd01
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2019-09-14 00:07:52 +0000

    can calculate perimeter and area

---
+++ area_and_perimeter.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+
+# Created by: Cameron Teed
+# Created On: September 2019
+# This program calculates the area and perimeter of a rectangle with the
+#   dimensions 5cm x 5cm
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("5cm x 5cm")
+    print("")
+    print("Area is {}cm^2".format(5*5))
+    print("Perimeter is {}cm".format(2*(5+5)))
+
+
+if __name__ == "__main__":
+    main()
